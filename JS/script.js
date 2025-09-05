be91f8489658e1de5e2df5ba8a8b0fde2c63246e
Aliaksei <aliakseimatsiyuk@gmail.com>
2025-09-05 16:12:23 +0200

    changed object in part 2

--- JS/script.js
+++ JS/script.js
@@ -7,14 +7,16 @@ console.log(numberOfFilms)
 //3)
 let lastWatchedMovie = prompt("Один из последних просмотренных фильмов?", "")
 let filmGrade = prompt("На сколько оцените его?", "")
-
+console.log(lastWatchedMovie)
+console.log(filmGrade)
 //2)
 const personalMovieDB = {
     count: numberOfFilms,
-    movies: {
-        lastWatchedMovie: filmGrade
-    },
+    movies: {lastWatchedMovie: lastWatchedMovie, filmGrade: filmGrade},
     actors: {},
     genres: [], 
     privat: false
 }
+// personalMovieDB.movies.lastWatchedMovie = lastWatchedMovie
+// personalMovieDB.movies.filmGrade = filmGrade
+console.log(personalMovieDB)
