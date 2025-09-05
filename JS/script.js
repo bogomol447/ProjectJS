c5479a57410d39d5ad9417b109a76c27b3a35a17
Aliaksei <aliakseimatsiyuk@gmail.com>
2025-09-05 16:39:34 +0200

    remove some lines with consol log

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,6 @@
 //1)
-let answer = prompt("Сколько фильмов вы уже просмотрели?", "")
+let numberOfFilms = prompt("Сколько фильмов вы уже просмотрели?", "")
 
-let numberOfFilms = answer
 console.log(numberOfFilms)
 
 //3)
@@ -9,10 +8,7 @@ let lastWatchedMovie = prompt("Один из последних просмотр
     filmGrade = prompt("На сколько оцените его?", ""),
     lastWatchedMovie2 = prompt("Один из последних просмотренных фильмов?", ""),
     filmGrade2 = prompt("На сколько оцените его?", "")
-console.log(lastWatchedMovie),
-            (filmGrade),
-            (lastWatchedMovie2),
-            (filmGrade2)
+
 //2)
 const personalMovieDB = {
     count: numberOfFilms,
@@ -21,6 +17,8 @@ const personalMovieDB = {
     genres: [], 
     privat: false
 }
+
 personalMovieDB.movies[lastWatchedMovie] = filmGrade
 personalMovieDB.movies[lastWatchedMovie2] = filmGrade2
+
 console.log(personalMovieDB)
