a231851c055fd0e80a5c87b16efbd127829ae2f4
Aliaksei <aliakseimatsiyuk@gmail.com>
2025-09-05 17:10:53 +0200

    finish a project

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,11 @@
-//1)
+
 let numberOfFilms = prompt("Сколько фильмов вы уже просмотрели?", "")
 
-//3)
 let lastWatchedMovie = prompt("Один из последних просмотренных фильмов?", ""),
     filmGrade = prompt("На сколько оцените его?", ""),
     lastWatchedMovie2 = prompt("Один из последних просмотренных фильмов?", ""),
     filmGrade2 = prompt("На сколько оцените его?", "")
 
-//2)
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
