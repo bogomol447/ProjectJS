4ae2d3749ca98edeebd87f57eb04f31098a2c876
Aliaksei <aliakseimatsiyuk@gmail.com>
2025-09-05 15:52:43 +0200

    add part 2

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,16 @@
+//1)
 let answer = prompt("Сколько фильмов вы уже просмотрели?", "")
 
 let numberOfFilms = answer
 console.log(numberOfFilms)
+
+//2)
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [], 
+    privat: false
+}
+
+//3)
