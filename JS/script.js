7f1c02e79d0a21009476fe00c493264fa761a468
Aliaksei <aliakseimatsiyuk@gmail.com>
2025-09-05 15:57:59 +0200

    add part 3, and changed part 2

--- JS/script.js
+++ JS/script.js
@@ -7,10 +7,14 @@ console.log(numberOfFilms)
 //2)
 const personalMovieDB = {
     count: numberOfFilms,
-    movies: {},
+    movies: {
+        lastWatchedMovie: filmGrade
+    },
     actors: {},
     genres: [], 
     privat: false
 }
 
 //3)
+let lastWatchedMovie = prompt("Один из последних просмотренных фильмов?", "")
+let filmGrade = prompt("На сколько оцените его?", "")
